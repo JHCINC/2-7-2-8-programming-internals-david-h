b16d7ff91185ef1901bf34e694c425b17a3d08e6
TohnGreatestName <144180804+TohnGreatestName@users.noreply.github.com>
2024-04-03 21:41:24 +0000

    play with raw input - will be necessary for accepting equations with superscript components

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,35 @@
+use std::io::{stdout, stdin};
+use std::io::Write;
+use crossterm::event::KeyCode;
+use crossterm::{cursor, style, terminal};
+use crossterm::{tty::IsTty, event::{Event, KeyEvent}};
+
 fn main() {
+
+    if !stdout().is_tty() {
+        todo!();
+    }
+
+    crossterm::terminal::enable_raw_mode().unwrap();
+
+    crossterm::execute!(stdout(), cursor::Hide, terminal::Clear(terminal::ClearType::All)).unwrap();
+
+    let mut s = String::new();
+    loop {
+        match crossterm::event::read().unwrap() {
+            Event::Key(KeyEvent {
+                code, ..
+            }) => {
+                if let KeyCode::Backspace = code {
+                    crossterm::terminal::disable_raw_mode().unwrap();
+                    break;
+                }
+                crossterm::execute!(stdout(), terminal::Clear(terminal::ClearType::CurrentLine), cursor::MoveTo(0, 0), style::Print(format!("{:?}", code))).unwrap();
+            }
+            _ => ()
+        }
+    }
+
+
     println!("Hello, world!");
 }
