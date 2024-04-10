accda196cc5c74ad6a0e3d8fdfb1c1d2e6ae1fce
TohnGreatestName <144180804+TohnGreatestName@users.noreply.github.com>
2024-04-10 00:13:58 +0000

    equations: add equation structure definitions

--- src/equations/mod.rs
+++ src/equations/mod.rs
@@ -1,1 +1,21 @@
+use std::num::NonZeroUsize;
+
+use crate::periodic_table::ElementNumber;
+
 mod parse;
+
+
+pub struct Component {
+    pub element: ElementNumber,
+    pub subscript: NonZeroUsize
+}
+pub struct EquationConstituent {
+    pub coefficient: NonZeroUsize,
+    pub components: Vec<Component>
+}
+
+#[derive(Default)]
+pub struct Equation {
+    pub reactants: Vec<EquationConstituent>,
+    pub products: Vec<EquationConstituent>
+}
